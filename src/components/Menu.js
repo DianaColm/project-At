bb6e6eebe7309a561485feee29075c1584d1f488
DianaColm <dcolmenerog@yahoo.com.mx>
2018-04-23 16:20:46 -0500

    subiendo primer componente

---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; //Este import va en todas las vistas, hace referencia a un pedacito de react
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="Menu">
+        
+
+      </div>
+    );
+  }
+}
+
+export default Menu;
